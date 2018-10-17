ab2b8dc5c4e2f7c7ca551fadb4ab50d610edc064
roman47 <dillonginley@gmail.com>
2018-10-16 23:03:33 -0400

    having some issues with the "show searched" items, still need to work out the bug

--- WorkLog.py
+++ WorkLog.py
@@ -1,7 +1,9 @@
 import os
 import csv
 import datetime
+import re
 
+WORK_LOG_FILE = 'workLog_records.csv'
 def show_menu_header(header):
     os.system('clear')
     print("______Work Log {}______".format(header))
@@ -28,9 +30,10 @@ def run_main_menu():
             break
         elif choice == 'l':
             print("You chose to lookup a new entry")
-            #display_find_menu()
-            display_entries()
+            display_find_menu()
+            #display_entries()
             break
+        # Menu has a “quit” option to exit the program.
         elif choice == 'q':
             print("We are sorry to see you go, goodbye.")
         else:
@@ -47,19 +50,23 @@ def display_find_menu():
 Find by [D]ate
 Find by [T]ime spent
 Find by [E]xact search
-Find by [P]attern"
+Find by [P]attern
 [R]eturn to Main Menu
 >""").lower()
         if choice == 'd':
-            print("You chose to search by date")
+            # print("You chose to search by date")
+            display_entries("d")
         elif choice == 't':
-            print("You chose to lookup by time spent")
+            # print("You chose to lookup by time spent")
+            display_entries("t")
         elif choice == 'e':
-            print("You chose to lookup by exact search")
+            # print("You chose to lookup by exact search")
+            display_entries("e")
         elif choice == 'p':
-            print("You chose to lookup by pattern")
+            # print("You chose to lookup by pattern")
+            display_entries("p")
         elif choice == 'r':
-            print("You chose to return to the main menu")
+            # print("You chose to return to the main menu")
             run_main_menu()
         else:
             print("You did not enter a valid choice, please only enter letters from the list")
@@ -69,7 +76,34 @@ def create_new_entry():
     # As a user of the script, if I choose to enter a new work log, I should be able to provide a task name, a number of
     # minutes spent working on it, and any additional notes I want to record.
     show_menu_header("Add New Entry")
-    #Make sure your script runs without errors. Catch exceptions and report errors to the user in a meaningful way.
+    entry_information = gather_entry_information(False)
+    with open(WORK_LOG_FILE, 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(entry_information)
+    run_main_menu()
+
+
+def overwrite_work_log(new_entry_information):
+    with open(WORK_LOG_FILE, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(["date", "task_title", "minutes", "notes"])
+        for entry in new_entry_information:
+            writer.writerow([entry["date"], entry["task_title"], str(entry["minutes"]), entry["notes"]])
+
+
+def gather_entry_information(includeDate):
+    date_str = datetime.date.today().strftime("%m/%d/%Y")
+    if includeDate:
+        while True:
+            try:
+                date_str = input("What is your date?>")
+                datetime.datetime.strptime(date_str, '%m/%d/%Y')
+            except ValueError:
+                print("Incorrect data format, should be MM/DD/YYYY")
+                continue
+            else:
+                break
+    # Make sure your script runs without errors. Catch exceptions and report errors to the user in a meaningful way.
     while True:
         task_title = input("What is your task title?>")
         if not task_title:
@@ -86,34 +120,144 @@ def create_new_entry():
         else:
             break
     notes = input("Please enter any optional notes>")
-    with open('workLog_records.csv', 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow([datetime.date.today().strftime("%m/%d/%Y"), task_title, minutes_spent, notes])
-    run_main_menu()
+    return [date_str, task_title, minutes_spent, notes]
 
 
-def display_entries():
+
+def load_work_log(search_type):
+    all_work_logs = []
+    with open(WORK_LOG_FILE, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            all_work_logs.append({"date": row["date"], "task_title": row["task_title"],
+                                  "minutes": row["minutes"], "notes": row["notes"], "show_log": 1})
+    # for log in all_work_logs:
+    # print(log["date"] + " " + str(log["show_log"]))
+    if search_type == 'd':
+        while True:
+            try:
+                date_str = input("For which MM/DD/YYYY date(s) do you want to search? (enter two separated by a space"
+                                 " if you want a range)>")
+                split_date_str = date_str.split()
+                if len(split_date_str) == 2:
+                    date1 = datetime.datetime.strptime(split_date_str[0], '%m/%d/%Y')
+                    date2 = datetime.datetime.strptime(split_date_str[1], '%m/%d/%Y')
+                    # print("The first date is " + split_date_str[0])
+                    # print("The second date is " + split_date_str[1])
+                    for log in all_work_logs:
+                        log["show_log"] = 1
+                        log_date = datetime.datetime.strptime(log["date"], '%m/%d/%Y')
+                        if date1 < date2:
+                            # print("date1 is earlier than date2 " + log["date"] + " " + str(log_date < date1) + " " + str(log_date > date2))
+                            if log_date < date1 or log_date > date2:
+                                log["show_log"] = 0
+                        elif date2 < date1:
+                            # print("date1 is later than date2 " + log["date"] + " " + str(log_date < date2) + " " + str(log_date > date1))
+                            if log_date < date2 or log_date > date1:
+                                log["show_log"] = 0
+                        else:
+                            # print("dates are equal, log date is " + log["date"] + " " + str(log_date != date1))
+                            if log_date != date1:
+                                log["show_log"] = 0
+
+                else:
+                    only_date = datetime.datetime.strptime(date_str, '%m/%d/%Y')
+                    # print("The only date is " + date_str)
+                    for log in all_work_logs:
+                        log["show_log"] = 1
+                        log_date = datetime.datetime.strptime(log["date"], '%m/%d/%Y')
+                        # print("log date is " + log["date"] + " " + str(log_date != only_date))
+                        if log_date != only_date:
+                            log["show_log"] = 0
+            except ValueError:
+                print("Incorrect data format, should be MM/DD/YYYY")
+                continue
+            else:
+                break
+                # for log in all_work_logs:
+                # print(log["date"] + " " + str(log["show_log"]))
+    elif search_type == 't':
+        while True:
+            try:
+                search_minutes = int(input("For how many minutes do you want to search?>"))
+                for log in all_work_logs:
+                    log["show_log"] = 1
+                    if int(log["minutes"]) != search_minutes:
+                        log["show_log"] = 0
+            except ValueError:
+                print("Please input an integer amount of minutes")
+                continue
+            else:
+                # for log in all_work_logs:
+                #     print(log["minutes"] + " " + str(log["show_log"]))
+                break
+    elif search_type == 'e':
+        search_exact = input("What is your exact search string?>")
+        for log in all_work_logs:
+            log["show_log"] = 1
+            if log["task_title"] != search_exact and log["notes"] != search_exact:
+                log["show_log"] = 0
+        # for log in all_work_logs:
+        #     print(log["task_title"] + " " + log["notes"] + " " + str(log["show_log"]))
+
+    elif search_type == 'p':
+        search_pattern = input("What is your pattern (regex) search string?>")
+        for log in all_work_logs:
+            log["show_log"] = 1
+            if not re.search(search_pattern, log["task_title"]) and not re.search(search_pattern, log["notes"]):
+                log["show_log"] = 0
+        # for log in all_work_logs:
+        #  print(log["task_title"] + " " + log["notes"] + " " + str(log["show_log"]))
+    return all_work_logs
+
+
+def display_entries(search_type):
     # When displaying the entries, the entries should be displayed in a readable format with the date, task name, time
     # spent, and notes information.
-    # Entries are displayed one at a time with the ability to page through records (previous/next/back).
+    work_logs = load_work_log(search_type)
     choice = ''
+    i = 0
     while choice != 'r':
-        show_menu_header("Display Entries")
-        print("The first entry is blah blah")
-        choice = input("""Please enter a letter from the choices below
-[E]dit current record
-[N]ext record
-[P]revious record
-[R]eturn to Main Menu
->""").lower()
+        # Entries are displayed one at a time with the ability to page through records (previous/next/back).
+        print("i: " + str(i) + " len(work_logs) " + str(len(work_logs)))
+        if (i < len(work_logs) and i >= 0):
+            log = work_logs[i]
+
+        if log["show_log"]:
+            show_menu_header("Display Entries")
+            print("Date: {} \nTask Title: {} \nMinutes Spent: {} \nNotes(Optional): {}"
+                .format(log["date"], log["task_title"], str(log["minutes"]), log["notes"]))
+            choice = input("""What would like you like to do? [E]dit record / [D]elete record / [N]ext record / [P]revious record / [R]eturn to Main Menu>""").lower()
+        #Entries can be deleted and edited, letting user change the date, task name, time spent, and/or notes.
         if choice == 'e':
             print("You chose to edit")
-        elif choice == 't':
-            print("You chose go to the next record")
-        elif choice == 'e':
-            print("You chose go to the previous record")
+            edited_entry_information = gather_entry_information(True)
+            log["date"] = edited_entry_information[0]
+            log["task_title"] = edited_entry_information[1]
+            log["minutes"] = edited_entry_information[2]
+            log["notes"] = edited_entry_information[3]
+        elif choice == 'd':
+            if len(work_logs) == 1:
+                print("You can't delete the last log")
+            else:
+                # print("You chose go to delete the record")
+                work_logs.remove(log)
+        # Entries are displayed one at a time with the ability to page through records (previous/next/back).
+        elif choice == 'n':
+            # print("You chose go to the next record")
+            if i == len(work_logs):
+                i = 0
+            else:
+                i += 1
+        elif choice == 'p':
+            # print("You chose go to the previous record")
+            if i == 0:
+                i = len(work_logs)
+            else:
+                i -= 1
         elif choice == 'r':
-            print("You chose to return to the main menu")
+            # print("You chose to return to the main menu")
+            overwrite_work_log(work_logs)
             run_main_menu()
         else:
             print("You did not enter a valid choice, please only enter letters from the list")
@@ -122,24 +266,3 @@ def display_entries():
 if __name__ == "__main__":
     run_main_menu()
 
-
-
-"""
-
-As a user of the script, if I choose to enter a new work log, I should be able to provide a task name, a number of minutes spent working on it, and any additional notes I want to record.
-
-
-Note:
-
-    When finding by date, I should be presented with a list of dates with entries and be able to choose one to see entries from.
-    When finding by time spent, I should be allowed to enter the number of minutes a task took and be able to choose one to see entries from.
-    When finding by an exact string, I should be allowed to enter a string and then be presented with entries containing that string in the task name or notes.
-    When finding by a pattern, I should be allowed to enter a regular expression and then be presented with entries matching that pattern in their task name or notes.
-
-
-
-Menu has a “quit” option to exit the program.
-Entries can be deleted and edited, letting user change the date, task name, time spent, and/or notes.
-Entries can be searched for and found based on a date range. For example between 01/01/2016 and 12/31/2016.
-Entries are displayed one at a time with the ability to page through records (previous/next/back).
-"""
